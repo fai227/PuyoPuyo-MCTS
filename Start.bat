93fefeb5dc85486e7017da3f50dd2e6a9a132085
fai227 <104518146+fai227@users.noreply.github.com>
2023-05-16 17:52:48 +0900

    Commit

---
+++ Start.bat
@@ -0,0 +1,25 @@
+rem move directory
+cd C:\Users\hayat\Documents\GitHub\PuyoPuyo-MCTS\src
+
+rem compile
+g++ MCTS.cpp -o ../MCTS
+
+rem move directory
+cd ../
+
+rem execute
+call MCTS.exe ^
+1000 ^
+20 ^
+5 ^
+1220000000000 ^
+1215000000000 ^
+3135000000000 ^
+4323150000000 ^
+4342315000000 ^
+2423311500000 ^
+2 2 ^
+1 1 ^
+15 ^
+0 ^
+0 0 0 0 0 0 0 0 0 10
